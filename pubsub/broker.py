e627576f26973b8bfbac0273186696b11f6ddbde
Kate Studzinski <kstudzinski@acm.org>
2021-05-24 15:26:26 -0400

    process registration

--- pubsub/broker.py
+++ pubsub/broker.py
@@ -22,15 +22,19 @@ class RoutingBroker(Broker):
     registration_req = context.socket(zmq.REQ)
     registration_rep = context.socket(zmq.REP)
 
+    poller = zmq.Poller()
+
     connect_address = None
+
+    topic2socket = {}
 
     def __init__(self, registration_address):
         self.connect_address = registration_address
         self.registration_req.connect(registration_address)
+        self.poller.register(self.registration_rep, zmq.POLLIN)
 
     def is_server(self):
-        reg_url = urlparse(self.connect_address)
-        bind_address = "{0}://*:{1}".format(reg_url.scheme, reg_url.port)
+        bind_address = self.bind_address(self.connect_address)
         self.registration_rep.bind(bind_address)
 
     def register_pub(self, topic, address):
@@ -48,6 +52,29 @@ class RoutingBroker(Broker):
 
     def process(self):
         """Polls for message on incoming connections and routes to subscribers"""
-        message = self.registration_rep.recv()
-        print(message)
-        self.registration_rep.send_string("received")
+        events = dict(self.poller.poll())
+        for socket in events.keys():
+            if self.registration_rep == socket:
+                reg_type, topic, address = self.registration_rep.recv().split()
+
+                if reg_type == "pub":
+                    socket = self.context.socket(zmq.SUB)
+                    socket.connect(address)
+                    socket.setsockopt(zmq.SUBSCRIBE, b"")
+                    self.poller.register(socket, zmq.POLLIN)
+                else:
+                    bind_address = self.bind_address(address.decode('utf-8'))
+                    if topic in self.topic2socket:
+                        socket = self.topic2socket[topic]
+                        socket.bind(bind_address)
+                    else:
+                        socket = self.context.socket(zmq.PUB)
+                        socket.bind(bind_address)
+                        self.topic2socket[topic] = socket
+
+                self.registration_rep.send_string("received")
+
+    @staticmethod
+    def bind_address(address):
+        bind_url = urlparse(address)
+        return "{0}://*:{1}".format(bind_url.scheme, bind_url.port)
