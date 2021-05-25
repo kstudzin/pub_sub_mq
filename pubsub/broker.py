ba016eaceef3145b074977a3542fb5f394dea97f
Kate Studzinski <kstudzinski@acm.org>
2021-05-25 17:49:21 -0400

    Broker has one socket with many connections

--- pubsub/broker.py
+++ pubsub/broker.py
@@ -25,6 +25,7 @@ class RoutingBroker(Broker):
     context = zmq.Context()
     registration_pub = context.socket(zmq.PUB)
     registration_sub = context.socket(zmq.SUB)
+    socket = context.socket(zmq.SUB)
 
     poller = zmq.Poller()
 
@@ -41,7 +42,9 @@ class RoutingBroker(Broker):
         self.registration_sub.bind(bind_address)
         self.registration_sub.setsockopt_string(zmq.SUBSCRIBE, REG_PUB)
         self.registration_sub.setsockopt_string(zmq.SUBSCRIBE, REG_SUB)
+
         self.poller.register(self.registration_sub, zmq.POLLIN)
+        self.poller.register(self.socket, zmq.POLLIN)
 
     def register_pub(self, topic, address):
         """Creates and returns a publisher with the given address. Saves a subscriber connection."""
@@ -67,30 +70,32 @@ class RoutingBroker(Broker):
 
     def process_registration(self, message):
         reg_type, topic, address = message.decode('utf-8').split()
+        logging.info(f"Broker processing {reg_type} to topic {topic} at address {address}")
 
         if reg_type == REG_PUB:
-            socket = self.context.socket(zmq.SUB)
-            socket.connect(address)
-            socket.setsockopt(zmq.SUBSCRIBE, b"")
-            self.poller.register(socket, zmq.POLLIN)
-            logging.info(f"Broker subscribed to topic {topic} on address {address}")
+            self.socket.connect(address)
+            self.socket.setsockopt_string(zmq.SUBSCRIBE, topic)
         elif reg_type == REG_SUB:
+
             if topic in self.topic2socket:
                 socket = self.topic2socket[topic]
             else:
                 socket = self.context.socket(zmq.PUB)
                 self.topic2socket[topic] = socket
 
+            logging.debug(f"Broker binding subscriber to socket {socket}")
             bind_address = util.bind_address(address)
             socket.bind(bind_address)
-            logging.info(f"Broker publishing topic {topic} on address {bind_address}. (socket {socket})")
 
     def process_message(self, message):
         decoded = message.decode('utf-8')
         logging.info(f"Broker received message: {decoded}")
 
         topic, value = decoded.split()
-        socket = self.topic2socket[topic]
+        if topic in self.topic2socket.keys():
+            socket = self.topic2socket[topic]
 
-        logging.info(f"Broker Sending message on topic {topic} to socket {socket}")
-        socket.send_string(decoded)
+            logging.info(f"Broker Sending message on topic {topic} to socket {socket}")
+            socket.send_string(decoded)
+        else:
+            logging.info(f"No subscribers listening for topic: {topic}")
