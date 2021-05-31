1e091972963f0eda46cde0b82ada8d8e21ddc720
Kate Studzinski <kstudzinski@acm.org>
2021-05-30 20:13:52 -0400

    Factor common code into AbstractBroker

--- pubsub/broker.py
+++ pubsub/broker.py
@@ -1,7 +1,8 @@
 import logging
+from abc import abstractmethod, ABC
+
 import zmq
 import pubsub
-from pubsub import util
 
 
 class BrokerType:
@@ -9,51 +10,14 @@ class BrokerType:
     ROUTE = "ROUTE"
 
 
-class Broker:
-
-    def process_registration(self):
-        pass
-
-
-class RoutingBroker(Broker):
-    """ Routing Broker implementation handles the routing of messages
-
-    The Routing Broker contains the following sockets:
-    - A socket that subscribes to registration messages
-    - Sockets that listen for incoming messages from publishers
-    - A socket that publishes received messages to subscribers
-
-    There are two key events that happen that this broker must respond to
-    - Registration messages from publishers and subscribers
-    - Receiving messages from publishers to route to subscribers
-
-    """
+class AbstractBroker(ABC):
     context = zmq.Context()
 
     def __init__(self, registration_address):
-        """ Creates a routing broker instance
-
-        :param registration_address: the address to use by this broker for publishers
-        and subscribers to register with. Format: <scheme>://<ip_addr>:<port>
-        """
         self.registration = self.context.socket(zmq.REP)
-        self.message_in = self.context.socket(zmq.SUB)
-        self.message_out = self.context.socket(zmq.PUB)
-
-        self.topic2message_out = {}
 
         self.connect_address = registration_address
         self.registration.bind(registration_address)
-
-    def process(self):
-        """Polls for incoming messages
-
-        Messages may be registration messages or publications.
-        """
-        message = self.message_in.recv_multipart()
-        logging.info(f"Received message: {message}")
-
-        self.message_out.send_multipart(message)
 
     def process_registration(self):
         """ Process registration messages
@@ -76,11 +40,60 @@ class RoutingBroker(Broker):
         logging.info(f"Broker processing {reg_type} to topic {topic} at address {address}")
 
         if reg_type == pubsub.REG_PUB:
-            self.message_in.connect(address)
-            self.message_in.setsockopt_string(zmq.SUBSCRIBE, topic)
+            self.process_pub_registration(topic, address)
         elif reg_type == pubsub.REG_SUB:
-            self.message_out.connect(address)
+            self.process_sub_registration(address)
         else:
             logging.warning(f"Received registration message with unknown type: {reg_type}")
 
         self.registration.send_string(BrokerType.ROUTE)
+
+    @abstractmethod
+    def process_pub_registration(self, topic, address):
+        pass
+
+    @abstractmethod
+    def process_sub_registration(self, address):
+        pass
+
+
+class RoutingBroker(AbstractBroker):
+    """ Routing Broker implementation handles the routing of messages
+
+    The Routing Broker contains the following sockets:
+    - A socket that subscribes to registration messages
+    - Sockets that listen for incoming messages from publishers
+    - A socket that publishes received messages to subscribers
+
+    There are two key events that happen that this broker must respond to
+    - Registration messages from publishers and subscribers
+    - Receiving messages from publishers to route to subscribers
+
+    """
+
+    def __init__(self, registration_address):
+        """ Creates a routing broker instance
+
+        :param registration_address: the address to use by this broker for publishers
+        and subscribers to register with. Format: <scheme>://<ip_addr>:<port>
+        """
+        super().__init__(registration_address)
+        self.message_in = self.context.socket(zmq.SUB)
+        self.message_out = self.context.socket(zmq.PUB)
+
+    def process(self):
+        """Polls for incoming messages
+
+        Messages may be registration messages or publications.
+        """
+        message = self.message_in.recv_multipart()
+        logging.info(f"Received message: {message}")
+
+        self.message_out.send_multipart(message)
+
+    def process_pub_registration(self, topic, address):
+        self.message_in.connect(address)
+        self.message_in.setsockopt_string(zmq.SUBSCRIBE, topic)
+
+    def process_sub_registration(self, address):
+        self.message_out.connect(address)
