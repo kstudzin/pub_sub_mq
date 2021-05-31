30b955fddc95b942809ecc054334e46aa8c6de72
ChandlerB <kelly@fairmountainarts.com>
2021-05-31 14:21:37 -0400

    Add CLI options for brokers

--- psserver.py
+++ psserver.py
@@ -1,5 +1,22 @@
 import threading
-import pubsub.broker
+import pubsub.broker as br
+import argparse as ap
+
+
+def config_parser() -> ap.ArgumentParser:
+    """
+    Configures the arguments accepted by the argparse module.
+    :return: A (argparse.ArgumentParser)
+    """
+    parser = ap.ArgumentParser(prog='Server', usage='%(prog)s [options]',
+                               description='Start broker service.')
+    parser.add_argument('--type', metavar='Type', type=str, nargs='?',
+                        help="Type of broker: 'd' direct 'r' router")
+    parser.add_argument('--address', metavar='Address', type=str, nargs='?',
+                        help='IP address EX: 127.0.0.1')
+    parser.add_argument('--port', metavar='Port', type=int, nargs='?',
+                        help='Port number EX: 5556')
+    return parser
 
 
 def msg_proc_loop(broker):
@@ -7,8 +24,8 @@ def msg_proc_loop(broker):
         broker.process()
 
 
-def main():
-    broker = pubsub.broker.RoutingBroker("tcp://127.0.0.1:5555")
+def routing_broker(address="127.0.0.1", port=5555):
+    broker = br.RoutingBroker(address, port)
     process_msg_thread = threading.Thread(target=msg_proc_loop,
                                           args=[broker],
                                           daemon=True)
@@ -16,6 +33,28 @@ def main():
 
     while True:
         broker.process_registration()
+
+
+def direct_broker(address="127.0.0.1", port=5555):
+    broker = br.DirectBroker(address, port)
+    process_msg_thread = threading.Thread(target=msg_proc_loop,
+                                          args=[broker],
+                                          daemon=True)
+    process_msg_thread.start()
+
+    while True:
+        broker.process_registration()
+
+
+def main():
+    arg_parser = config_parser()
+    ps_args = arg_parser.parse_args()
+    if ps_args.type == "r":
+        routing_broker(ps_args.address, ps_args.port)
+    elif ps_args.type == "d":
+        direct_broker(ps_args.address, ps_args.port)
+    else:
+        print("Invalid option")
 
 
 if __name__ == "__main__":
