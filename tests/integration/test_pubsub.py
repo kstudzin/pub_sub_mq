f46be2cc9c095dbc01e9d9a1a68409d4c98ff85f
Kate Studzinski <kstudzinski@acm.org>
2021-06-03 17:33:41 -0400

    Add tests with publisher reg first

--- tests/integration/test_pubsub.py
+++ tests/integration/test_pubsub.py
@@ -91,6 +91,70 @@ def test_publish_direct():
     assert nl == numbers
 
 
+def test_publish_first_routing():
+    topic = "numbers"
+    num_msg = 100
+    nl.clear()
+
+    broker = RoutingBroker("tcp://127.0.0.1:5562")
+    executor.submit(wait_loop, broker.process, num_msg)
+    executor.submit(wait_loop, broker.process_registration, 2)
+
+    logging.info("setting up publisher")
+    pub = Publisher(pub_address, broker_address)
+    pub.register(topic)
+
+    logging.info("setting up subscriber")
+    sub = Subscriber(sub_address, broker_address)
+    future = executor.submit(wait_loop, sub.wait_for_msg, num_msg)
+
+    sub.register_callback(add_number)
+    sub.register(topic)
+    sleep(.5)
+
+    numbers = []
+    for i in range(num_msg):
+        pub.publish(topic, str(i))
+        numbers.append(str(i))
+
+    future.result(60)
+    logging.info(f"nl: {nl}")
+    logging.info(f"nu: {numbers}")
+    assert nl == numbers
+
+
+def test_publish_first_direct():
+    topic = "numbers"
+    num_msg = 100
+    nl.clear()
+
+    broker = DirectBroker("tcp://127.0.0.1:5565")
+    executor.submit(wait_loop, broker.process_registration, 3)
+
+    logging.info("setting up publisher")
+    pub = Publisher(pub_address, "tcp://127.0.0.1:5565")
+    pub.register(topic)
+
+    logging.info("setting up subscriber")
+    sub = Subscriber(sub_address, "tcp://127.0.0.1:5565")
+    future = executor.submit(wait_loop, sub.wait_for_msg, num_msg)
+    executor.submit(wait_loop, sub.wait_for_registration, 1)
+
+    sub.register_callback(add_number)
+    sub.register(topic)
+
+    sleep(.5)
+    numbers = []
+    for i in range(num_msg):
+        pub.publish(topic, str(i))
+        numbers.append(str(i))
+
+    future.result(60)
+    logging.info(f"nl: {nl}")
+    logging.info(f"nu: {numbers}")
+    assert nl == numbers
+
+
 def test_two_publishers():
     topic = "numbers"
     topic2 = "other numbers"
