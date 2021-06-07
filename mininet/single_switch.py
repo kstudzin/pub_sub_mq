891d67f66071921015458f56df2a90d7a48a732e
Kate Studzinski <kstudzinski@acm.org>
2021-06-07 13:21:59 -0400

    Setup pubsub in mininet

--- mininet/single_switch.py
+++ mininet/single_switch.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+from time import sleep
+
 from mininet.cli import CLI
 from mininet.node import OVSController
 from mininet.topo import Topo
@@ -6,6 +8,11 @@ from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 
+address_format = "tcp://{0}:{1}"
+default_port = "5555"
+broker_cmd = "python ../psserver --address {0} --port {1} --type {2}"
+publisher_cmd = "python ../ps_publisher.py {0} {1} --topics lorem -r 1000"
+subscriber_cmd = "python ../ps_publisher.py {0} {1} --topics lorem "
 
 class SingleSwitchTopo(Topo):
     """Single switch connected to n hosts."""
@@ -30,7 +37,23 @@ def simple_test():
     print("Testing network connectivity")
     net.pingAll()
 
-    CLI(net)
+    hosts = net.hosts
+    broker = hosts[0]
+    publisher = hosts[1]
+    subscriber = hosts[2]
+
+    broker_address = address_format.format(broker.IP, default_port)
+    broker.cmd(broker_cmd.format(broker.IP, default_port, 'r'))
+    publisher.cmd(publisher_cmd.format(
+        address_format.format(publisher.IP, default_port),
+        broker_address
+        ))
+    sleep(.05)
+    subscriber.cmd(subscriber_cmd.format(
+        address_format.format(subscriber.IP, default_port),
+        broker_address
+    ))
+    # CLI(net)
 
     net.stop()
 
