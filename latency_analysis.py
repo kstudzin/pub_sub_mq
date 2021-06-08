4fd73d9a8917662c633054a6007576748806c02f
Kate Studzinski <kstudzinski@acm.org>
2021-06-08 11:46:41 -0400

    Clean names

--- latency_analysis.py
+++ latency_analysis.py
@@ -1,21 +1,24 @@
 import os
 import pathlib
+import re
 
 import pandas as pd
 from matplotlib import pyplot as plt
 
 source = "latency"
+filename_pattern = re.compile(".*(sub-(\\d+)_broker-([rd])).*")
 
 
 def import_data():
     df = pd.DataFrame()
     names = []
     for file in os.listdir(source):
-        name = pathlib.Path(file).stem
+        match = filename_pattern.match(file)
+        name = "{0:03}_{1}".format(int(match.group(2)), match.group(3))
         df_curr = pd.read_csv(os.path.join(source, file), header=None, usecols=[0], names=[name])
         df[name] = df_curr[name]
         names.append(name)
-    return df, names
+    return df, names.sort()
 
 
 def main():
