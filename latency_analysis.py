0aba948d97be6025ec0cf1dd8dffa9852448f0ac
Kate Studzinski <kstudzinski@acm.org>
2021-06-08 08:59:10 -0400

    Add boxplot drawing to latency analysis

--- latency_analysis.py
+++ latency_analysis.py
@@ -2,22 +2,28 @@ import os
 import pathlib
 
 import pandas as pd
+from matplotlib import pyplot as plt
 
 source = "latency"
 
 
 def import_data():
     df = pd.DataFrame()
+    names = []
     for file in os.listdir(source):
         name = pathlib.Path(file).stem
         df_curr = pd.read_csv(os.path.join(source, file), header=None, usecols=[0], names=[name])
         df[name] = df_curr[name]
-    return df
+        names.append(name)
+    return df, names
 
 
 def main():
-    df = import_data()
-    print(df.head(10))
+    df, cols = import_data()
+
+    plt.figure()
+    df.boxplot(column=cols)
+    plt.savefig("test.png", format="png")
 
 
 if __name__ == "__main__":
